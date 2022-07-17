c4f90c5db6d72d93ccedc1d2ad934cecdd64e7b3
Jongseung Kim <jngsngkm@gmail.com>
2022-07-18 00:29:39 +0900

    Http Protocol, Asynchronous Programming, XML

--- Weather/WeatherExample.cs
+++ Weather/WeatherExample.cs
@@ -1,20 +1,57 @@
 ﻿
+using System.Xml;
+
 public class WeatherExample
 {
     public async Task RunAsync()
     {
         HttpClient client = new HttpClient();
-        HttpResponseMessage response = await client.GetAsync("https://www.weather.go.kr/w/index.do");
+        HttpResponseMessage response = await client.GetAsync("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=156");
 
-        // 오류검사
+        // Error
         if (!response.IsSuccessStatusCode)
         {
-            Console.WriteLine($"서버에서 오류를 반환했습니다 반환코드 = {response.StatusCode}");
+            Console.WriteLine($"Error Return Code = {response.StatusCode}");
             return;
         }
 
-        // 오류가 없다면
+        // RSS data
         string Contents = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(Contents);
+
+        // XML 파싱
+        XmlDocument document  = new XmlDocument();
+        document.LoadXml(Contents);
+
+        // Location
+        var nodes = document.DocumentElement.SelectNodes("descendant :: location");
+
+        foreach(XmlNode node in nodes)
+        {
+            var provinceNode = node.SelectSingleNode("province");
+            var cityNode = node.SelectSingleNode("city");
+
+            if (provinceNode == null || cityNode == null) continue;
+
+            if (provinceNode.InnerText != "광주ㆍ전라남도" || cityNode.InnerText != "나주") continue;
+
+            PrintNode(node);
+
+            break;
+        }
+
+    }
+
+    private static void PrintNode(XmlNode sourceNode)
+    {
+        var nodes = sourceNode.SelectNodes("descendant :: data");
+        foreach(XmlNode node in nodes)
+        {
+            var dataNode = node.SelectSingleNode("tmEf");
+            var weatherNode = node.SelectSingleNode("wf");
+
+            if(dataNode == null || weatherNode == null) continue;
+
+            Console.WriteLine($"날짜 : {dataNode.InnerText}, 날씨 : {weatherNode.InnerText}");
+        }
     }
 }
